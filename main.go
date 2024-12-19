b577cf37091f158c7049dfb9c550379707bd1e3a
cooler-SAI <cooler19860212@yahoo.com>
2024-12-19 19:37:12 +0300

    add base addresses

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"net/http"
 	"time"
 )
 
 func main() {
 	fmt.Println("Hello go-http")
 	fmt.Println(time.Now())
-	fmt.Println("Please press any key to continue....")
 
-	reader := bufio.NewReader(os.Stdin)
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fprintf, err := fmt.Fprintf(w, "Hello go-http")
+		if err != nil {
+			fmt.Println(fprintf)
+			return
+		}
+	})
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fprintln2, err := fmt.Fprintln(w, "This is the About page of the Simple Go App.")
+		if err != nil {
+			fmt.Println(fprintln2)
+			return
+		}
+	})
 
 }
