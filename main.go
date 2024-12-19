63b15c9c995a50bcc36a85870a01875d8dcdc622
cooler-SAI <cooler19860212@yahoo.com>
2024-12-19 21:07:19 +0300

    added prometheus in project

--- main.go
+++ main.go
@@ -8,18 +8,35 @@ import (
 	"os"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
+var (
+	httpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total HTTP Requests",
+		},
+		[]string{"path"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(httpRequestsTotal)
+}
+
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: zerolog.ConsoleWriter{Out: os.Stdout}})
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
 
 	log.Info().Msg("Hello go-http")
 	log.Info().Time("timestamp", time.Now()).Msg("Server is starting")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		httpRequestsTotal.WithLabelValues("/").Inc()
 		_, err := fmt.Fprintf(w, "Hello go-http")
 		if err != nil {
 			log.Error().Err(err).Msg("Error writing response to /")
@@ -28,12 +45,15 @@ func main() {
 	})
 
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		httpRequestsTotal.WithLabelValues("/about").Inc()
 		_, err := fmt.Fprintln(w, "This is the About page of the Simple Go App.")
 		if err != nil {
 			log.Error().Err(err).Msg("Error writing response to /about")
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		}
 	})
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	server := &http.Server{
 		Addr:         ":8080",
